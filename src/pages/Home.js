5a557fa9e846fb6c761b95505df9df801da55f87
MattCornicDev <glgharib@gmail.com>
2022-10-09 17:28:04 +0200

    correction du text de presentation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
                     <h2>Devéno</h2>
 
                     <p>Devéno est le fruit d'une collaboration entre deux développeurs originaire de la province du grand Hainaut  et partageant la même passion pour le developpement d'applications web, mobile et jeux vidéos.<br/>
-                        S'estimant chanceux vivre une période prospère en matière de technologie permettant d'améliorer le quotidien comme jamais, nous avons decidé de joindre nos efforts et compétences pour multiplier nos ressources.<br/>
+                        S'estimant chanceux de vivre une période prospère en matière de technologie permettant d'améliorer le quotidien comme jamais, nous avons decidé de joindre nos efforts et compétences pour multiplier nos ressources.<br/>
                         Le développement informatique évolue constament, si vous avez besoin de maintenir vos APIs à jour, Si vous êtes à la recherche de développeurs compétents, passionnés maitrisant le travaille d'équipe. Nous serons ravi de contribuer à vos projets.
                         <br/><br/>
                     </p>
