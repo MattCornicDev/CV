82892bee9b83a69609d31e85eff5564bf681f1ed
MattCornicDev <glgharib@gmail.com>
2022-10-09 13:34:41 +0200

    Navbar avec Elodie et Rarib

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -5,33 +5,40 @@ const Experience = () => {
         <div className="experience">
             <h3>Experience</h3>
             <div className="exp-1">
+                <h4>GRETA Grand Hainaut</h4>
+                <h5>2022 - Septembre</h5>
+                <p>
+                    Formateur JavaScript, nodeJS et ReactJS
+                </p>
+            </div>
+            <div className="exp-2">
                 <h4>Suez Smart Solution</h4>
                 <h5>2021 - Juillet à Novembre</h5>
                 <p>
                     Développement d'une application PHP Symfony
                 </p>
             </div>
-            <div className="exp-2">
+            <div className="exp-3">
                 <h4>One Foncy</h4>
                 <h5>2019 - juillet - septembre</h5>
                 <p>Développement d'une application node.js</p>
             </div>
-            <div className="exp-3">
+            <div className="exp-4">
                 <h4>Developpement projets Symfony</h4>
                 <h5>2019 - 2022</h5>
                 <p>Développement d'une application Symfony divers</p>
             </div>
-            <div className="exp-4">
+            <div className="exp-5">
                 <h4>Responsable Agent pour refrisol</h4>
                 <h5>2010 - 2018</h5>
                 <p>Développement d'une application Symfony divers</p>
             </div>
-            <div className="exp-5">
+            <div className="exp-6">
                 <h4>Gérant société La Diligence</h4>
                 <h5>2005 - 2010</h5>
                 <p>Société de transport de véhicule de moins de 2,5 tonnes</p>
             </div>
-            <div className="exp-6">
+            <div className="exp-7">
                 <h4>Assistant d'éducation</h4>
                 <h5>2003 - 2005</h5>
                 <p>Assistant d'éducation pour le ministère de l'éducation nationale</p>
