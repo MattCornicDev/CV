d0d893c059b22cd7a3e6dedf9b142ed69f9d9756
MattCornicDev <glgharib@gmail.com>
2022-10-13 15:31:02 +0200

    sauvegarde

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
 
                     <h2>Devéno</h2>
 
-                    <p>Devéno est le fruit d'une collaboration entre deux développeurs originaire de la province du grand Hainaut  et partageant la même passion pour le developpement d'applications web, mobile et jeux vidéos.<br/>
-                        S'estimant chanceux de vivre une période prospère en matière de technologie permettant d'améliorer le quotidien comme jamais, nous avons decidé de joindre nos efforts et compétences pour multiplier nos ressources.<br/>
+                    <p>Devéno est le fruit d'une collaboration entre deux développeurs originaire de la province du grand Hainaut  et partageant la même passion pour le developpement d'applications web, mobile et jeux vidéos.<br/><br/>
+                        S'estimant chanceux de vivre une période prospère en matière de technologie permettant d'améliorer le quotidien comme jamais, nous avons decidé de joindre nos efforts et compétences pour multiplier nos ressources.<br/><br/>
                         Le développement informatique évolue constament, si vous avez besoin de maintenir vos APIs à jour, Si vous êtes à la recherche de développeurs compétents, passionnés maitrisant le travaille d'équipe. Nous serons ravi de contribuer à vos projets.
                         <br/><br/>
                     </p>
@@ -20,8 +20,8 @@ const Home = () => {
 
 
                     <div className="pdf">
-                        <a href="./media/resume.pdf" target="_blank">CV de Rarib LAAMIMAT</a>
-                        <a href="./media/CV.Elodie-Ksiazek.pdf" target="_blank">CV d'Elodie KSIAZEK</a>
+                        <a href="./media/resume.pdf" target="_blank">CV Rarib LAAMIMAT</a>
+                        <a href="./media/CV.Elodie-Ksiazek.pdf" target="_blank">CV Elodie KSIAZEK</a>
                     </div>
                 </div>
             </div>
